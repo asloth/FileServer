995f8796220c9274f2145f372fcc58c81eb19970
asloth <sbenelramirez@gmail.com>
2022-07-26 00:23:47 -0500

    add command class

---
+++ models/command.go
@@ -0,0 +1,20 @@
+package models
+
+// A enum is defined with all the commands the server accept
+type ID int
+
+const (
+	REGISTER   ID = iota + 1 //Command for registering a client in the server
+	SUSCRIBE                 // Command for suscribe to a channel
+	UNSUSCRIBE               // Command for unsuscribe to a channel
+	SEND                     // Command for send a file to a channel
+	LCHANNELS                //Command for listing all the channels
+)
+
+// A class command is defined and its params as class attributes
+type Command struct {
+	id      ID     // The command
+	sender  string // The id of the sender that is sending the file
+	channel string // The name of the channel to send the file to
+	body    []byte // The file
+}
