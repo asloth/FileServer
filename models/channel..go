df154e0672cd2d2e388776576898d30e65007905
asloth <sbenelramirez@gmail.com>
2022-07-27 05:21:56 -0500

    feat sending files through channels

--- models/channel..go
+++ models/channel..go
@@ -1,34 +1,67 @@
 package models
 
+import (
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+)
+
+const BUFFERSIZE = 1024
+
 type Channel struct {
 	name    string           //Name of the channel
 	clients map[*Client]bool //All the clients that are suscribe to the channel
 }
 
 func (c *Channel) broadcast(s string, m []byte) {
-	msg := append([]byte(s), ": "...)
-	msg = append(msg, m...)
-	msg = append(msg, '\n')
+	// msg := append([]byte(s), ": "...)
+	// msg = append(msg, m...)
+	// msg = append(msg, '\n')
+	filePath := string(m)
 
 	for cl := range c.clients {
+		connection := cl.Conn
 
 		if cl.username != s {
-			cl.Conn.Write(msg)
+			file, err := os.Open(filePath)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			fileInfo, err := file.Stat()
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			fileSize := fillString(strconv.FormatInt(fileInfo.Size(), 10), 10)
+			fileName := fillString(fileInfo.Name(), 64)
+			fmt.Println("Sending filename and filesize!")
+			connection.Write([]byte(fileSize))
+			connection.Write([]byte(fileName))
+			sendBuffer := make([]byte, BUFFERSIZE)
+			fmt.Println("Start sending file!")
+			for {
+				_, err = file.Read(sendBuffer)
+				if err == io.EOF {
+					break
+				}
+				connection.Write(sendBuffer)
+			}
+			fmt.Println("File has been sent")
 		}
 	}
 
-	/* buf := make([]byte, 1024)
-	// for {
-	//     n, err := c.Conn.Read(buf)
-	//     if err != nil {
-	//         if err != io.EOF {
-	//             log.Println(err)
-	//         }
+}
 
-	//         return
-	//     }
-	//     log.Printf("received: %q", buf[:n])
-	//     log.Printf("bytes: %d", n)
-
-	 }*/
+func fillString(retunString string, toLength int) string {
+	for {
+		lengtString := len(retunString)
+		if lengtString < toLength {
+			retunString = retunString + ":"
+			continue
+		}
+		break
+	}
+	return retunString
 }
