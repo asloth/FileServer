43e1deafbf5a6c389f4f6ea84f0e94d66b483390
asloth <sbenelramirez@gmail.com>
2022-07-29 00:10:13 -0500

    fix: broadcast method just write in the connection

--- models/channel..go
+++ models/channel..go
@@ -1,14 +1,5 @@
 package models
 
-import (
-	"fmt"
-	"io"
-	"os"
-	"strconv"
-)
-
-const BUFFERSIZE = 1024
-
 type Channel struct {
 	name    string           //Name of the channel
 	clients map[*Client]bool //All the clients that are suscribe to the channel
@@ -18,50 +9,13 @@ func (c *Channel) broadcast(s string, m []byte) {
 	// msg := append([]byte(s), ": "...)
 	// msg = append(msg, m...)
 	// msg = append(msg, '\n')
-	filePath := string(m)
 
 	for cl := range c.clients {
 		connection := cl.Conn
 
 		if cl.username != s {
-			file, err := os.Open(filePath)
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
-			fileInfo, err := file.Stat()
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
-			fileSize := fillString(strconv.FormatInt(fileInfo.Size(), 10), 10)
-			fileName := fillString(fileInfo.Name(), 64)
-			fmt.Println("Sending filename and filesize!")
-			connection.Write([]byte(fileSize))
-			connection.Write([]byte(fileName))
-			sendBuffer := make([]byte, BUFFERSIZE)
-			fmt.Println("Start sending file!")
-			for {
-				_, err = file.Read(sendBuffer)
-				if err == io.EOF {
-					break
-				}
-				connection.Write(sendBuffer)
-			}
-			fmt.Println("File has been sent")
+			connection.Write(m)
 		}
 	}
 
-}
-
-func fillString(retunString string, toLength int) string {
-	for {
-		lengtString := len(retunString)
-		if lengtString < toLength {
-			retunString = retunString + ":"
-			continue
-		}
-		break
-	}
-	return retunString
 }
